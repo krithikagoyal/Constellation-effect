7d1de59dd9709182ad374d7253bd132f42947853
Krithika <goyalkrithika7@gmail.com>
2021-09-03 14:03:20 +0530

    constellation made

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ window.addEventListener('resize', function (e) {
     canvas.height = window.innerHeight;
 });
 
+let hueCol = 0;
+
 const mouse = {
     x: undefined,
     y: undefined,
@@ -17,36 +19,72 @@ const mouse = {
 canvas.addEventListener('click', function (e) {
     mouse.x = e.x;
     mouse.y = e.y;
+    for (let i = 0; i < 5; i++) {
+        particles.push(new Particle);
+    }
 })
 
 canvas.addEventListener('mousemove', function (e) {
     mouse.x = e.x;
     mouse.y = e.y;
+    for (let i = 0; i < 5; i++) {
+        particles.push(new Particle);
+    }
 })
 
 class Particle {
     constructor() {
         this.x = mouse.x;
         this.y = mouse.y;
-        this.speed = Math.random();
-        this.color = ;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = 'hsl(' + hueCol + ', 100%, 50%)';
+        this.size = Math.random() * 5 + 1;
     }
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.size -= 0.1;
     }
     draw() {
+        ctx.fillStyle = this.color;
         ctx.beginPath();
-
-        ctx.arc(this.x, this.y, 0, Math.PI * 2);
-        ctx.fillStyle;
+        ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
+        ctx.fill();
     }
 }
 
 function handleParticles() {
-    
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+        for (var j = i + 1; j < particles.length; j++) {
+            const dx = particles[j].x - particles[i].x;
+            const dy = particles[j].y - particles[i].y;
+            const distance = dx * dx + dy * dy;
+            if (distance < 10000) {
+                ctx.beginPath();
+                ctx.strokeStyle = particles[i].color;
+                ctx.lineWidth = 0.3;
+                ctx.moveTo(particles[i].x, particles[i].y);
+                ctx.lineTo(particles[j].x, particles[j].y);
+                ctx.stroke();
+            }
+        }
+        if (particles.size < 0.3) {
+            particles.splice(i, 1);
+            i--;
+        }
+    }
 }
 
 function animate() {
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    hueCol += 2;
+    requestAnimationFrame(animate);
 }
+
+animate();
